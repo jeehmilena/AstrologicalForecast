05299986165312d0aa8c69d227147a99b4fdc952
jeehmilena <jessicamilenacorrea@gmail.com>
2020-08-26 17:15:30 -0300

    protótipo splash

---
+++ app/src/main/java/com/example/kosmos/splash/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kosmos.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kosmos.R
+import com.example.kosmos.chooseyoursign.view.ChooseYourSignActivity
+import java.util.*
+
+class SplashActivity : AppCompatActivity() {
+    val timer = Timer()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                jump()
+            }
+        }, 3000)
+    }
+
+    private fun jump() {
+        timer.cancel()
+        startActivity(Intent(this@SplashActivity, ChooseYourSignActivity::class.java))
+        finish()
+    }
+}
